101f429327db7393373c950d70c5c48878e53415
klebersin <kleber.sik@gmail.com>
2020-07-22 08:52:01 -0500

    first commit

---
+++ src/app/components/components-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ComponentsPage } from './components.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ComponentsPage
+  },
+  {
+    path: 'services',
+    loadChildren: () => import('./services/services.module').then( m => m.ServicesPageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ComponentsPageRoutingModule {}
